6cded59d17ceb568d387ea86590acc5110f7b92f
Vitalii <vitalik.shchudlo01@ukr.net>
2022-03-11 23:19:50 +0200

    Improved cogs

--- bot.py
+++ bot.py
@@ -5,36 +5,7 @@ from data import config
 
 bot = commands.Bot(command_prefix=('.', ';'), intents=discord.Intents.all())
 
-
-@bot.command()
-@commands.is_owner()
-async def reload(ctx, *, name: str):
-    try:
-        bot.reload_extension(f"cogs.{name}")
-    except Exception as e:
-        return await ctx.send(e)
-    await ctx.send(f'"**{name}**" Cog reloaded')
-
-
-@bot.command()
-@commands.is_owner()
-async def unload(ctx, *, name: str):
-    try:
-        bot.unload_extension(f"cogs.{name}")
-    except Exception as e:
-        return await ctx.send(e)
-    await ctx.send(f'"**{name}**" Cog unloaded')
-
-
-@bot.command()
-@commands.is_owner()
-async def load(ctx, *, name: str):
-    try:
-        bot.load_extension(f"cogs.{name}")
-    except Exception as e:
-        return await ctx.send(e)
-    await ctx.send(f'"**{name}**" Cog loaded')
-
+bot.load_extension("cogs.cogs_manager")
 
 bot.load_extension("cogs.achivements.achivements")
 bot.load_extension("cogs.events.on_ready")
