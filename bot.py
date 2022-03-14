50aaba5e2025f17f39fba382dc5058702a7ce41c
Vitalii <vitalik.shchudlo01@ukr.net>
2022-03-15 00:41:51 +0200

    Improved the config files. Refactored all system working with IDs not with nicknames of the users

--- bot.py
+++ bot.py
@@ -2,15 +2,17 @@ import discord
 from discord.ext import commands
 
 from data import config
+import os
+os.system('clear')
 
 bot = commands.Bot(command_prefix=('.', ';'), intents=discord.Intents.all())
 
-
+bot.load_extension("cogs.events.on_ready")
 
 bot.load_extension("cogs.cogs_manager")
 
 bot.load_extension("cogs.achivements.achivements")
-bot.load_extension("cogs.events.on_ready")
 bot.load_extension("cogs.system.clear_chat")
 bot.load_extension("cogs.system.json")
+
 bot.run(config.BOT_TOKEN)
--- cogs/events/on_ready.py
+++ cogs/events/on_ready.py
@@ -1,7 +1,9 @@
+from discord import Game
 from discord.ext import commands
-from discord import Game, Activity, ActivityType, Streaming
+
 from data import config
 from discord_bot_wefi.database.db import Database
+
 
 class OnReady(commands.Cog):
     def __init__(self, bot):
@@ -10,36 +12,39 @@ class OnReady(commands.Cog):
     @commands.Cog.listener()  # You use commands.Cog.listener() instead of bot.event
     async def on_ready(self):
         print('Loading bot...')
-        achivement_role = config.ACHIVEMENT_ROLE_NAME
-        all_roles = self.bot.guilds[0].roles
+        achievement_role_id = str(config.ID_ROLE_ACHIEVEMENTS)
         status_role = False
         users = []
 
-        print('\nUpdating users with achivement role...')
-        for role in all_roles:
-            if achivement_role in role.name:
+        print('\nChecking roles...OK')
+        for role in self.bot.guilds[0].roles:
+            if achievement_role_id in str(role.id):
                 status_role = True
         if not status_role:
-            # print(f'Starting....Error\nACHIVEMENT_ROLE_NAME - {achivement_role} does not exists\n')
-            exit(f'ACHIVEMENT_ROLE_NAME - {achivement_role} does not exists')
+            exit(f'Achievement role with ID: {achievement_role_id} - does not exists')
 
+        print('Updating users with achievement role...')
         for member in self.bot.guilds[0].members:
             for role in member.roles:
-                if str(role) == achivement_role:
+                if str(role.id) == achievement_role_id:
                     users.append(member)
-                    print(f'**{member.name}** ID({member.id}) - have role "{achivement_role}"')
+                    print(f'**{member.name}** ID({member.id}) - have role "{achievement_role_id}"')
         print('\n')
 
-        if not Database().reset_sequecne('achivement_users'):
+        if not Database().reset_sequecne('achievement_users'):
             print('Sequence updating...ERROR')
-        print('Sequence updating...SUCCESS')
+        else:
+            print('Sequence updating...OK')
 
         if not Database().save_achivement_users(users=users):  # Saving to the DB
-            exit('Something happen with DataBase')
-        print(f'{len(users)} users have been added to the DB')
+            # exit('Something happen with DataBase')
+            print('Something happen with DataBase')
+        else:
+            print('Updating DataBase...OK')
+            print(f'{len(users)} users have been added to the DB')
+
         print('\nStarting Bot...SUCCESS\n\n')
         await self.bot.change_presence(activity=Game('Beta version 1.0'))
-
 
 
 def setup(bot):
