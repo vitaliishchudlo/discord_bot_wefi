09d947450cd0ad4c6228b207d7cf4bc2368bbcbb
Vitalii <vitalik.shchudlo01@ukr.net>
2022-03-14 21:38:40 +0200

    Updated database - migrations. Also imporved clear command

--- cogs/system/clear_chat.py
+++ cogs/system/clear_chat.py
@@ -1,4 +1,9 @@
+import asyncio
+
 from discord.ext import commands
+from discord.ext.commands import MissingPermissions, has_any_role
+
+from data import config
 
 
 class ClearChat(commands.Cog):
@@ -6,9 +11,27 @@ class ClearChat(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def clear(self, ctx, amount=10):
+    @has_any_role('💚𝐖𝐞𝐅𝐢💚', config.MODERATOR_ROLE)
+    async def clear(self, ctx, amount=1):
+        defended_chats = config.DEFENDED_CHATS
+        if not bool(ctx.channel.name in defended_chats):
+            await ctx.message.delete()
+            return await ctx.channel.purge(limit=amount)
+        author_roles = [role.name for role in ctx.author.roles]
+        allowed_roles = config.ADMINS_ROLES
+        intersection_roles = list(set(author_roles).intersection(set(allowed_roles)))
+        if not intersection_roles:
+            msg_bot = await ctx.send("This chat is defended from your role!")
+            await asyncio.sleep(1.1)
+            await msg_bot.delete()
+            return await ctx.message.delete()
         await ctx.message.delete()
-        await ctx.channel.purge(limit=amount)
+        return await ctx.channel.purge(limit=amount)
+
+    @clear.error
+    async def clear_error(self, ctx, error):
+        if not isinstance(error, MissingPermissions):
+            await ctx.send("You don't have permission to use **clear** command!")
 
 
 def setup(bot):
