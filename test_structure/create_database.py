128c64ebe0719b2885506adf6f9ea73c6d455b5d
vitaliishchudlo <vitalik.shchudlo01@ukr.net>
2022-09-18 21:16:20 +0300

    workable structure for SQLAlchemy

--- test_structure/create_database.py
+++ test_structure/create_database.py
@@ -1,39 +1,5 @@
-# from test_structure.database import create_db
 from test_structure.database import create_db
 
-def create_database(load_fake_data: bool = True):
+
+def create_database():
     create_db()
-    # if load_fake_data:
-    #     _load_fake_data(Session())
-
-
-# def _load_fake_data(session: Session):
-#     lessons_names = ['Математика', 'Программирование', 'Философствуем за кружечкой пенного',
-#                      'Алгоритмы и структуры данных', 'Линейная алгебра', 'Мат. статистика',
-#                      'Физкультура']
-#     group1 = Group(group_name='1-МДА-7')
-#     group2 = Group(group_name='1-МДА-9')
-#     session.add(group1)
-#     session.add(group2)
-#
-#     for key, it in enumerate(lessons_names):
-#         lesson = Lesson(lesson_title=it)
-#         lesson.groups.append(group1)
-#         if key % 2 == 0:
-#             lesson.groups.append(group2)
-#         session.add(lesson)
-#
-#     faker = Faker('ru_RU')
-#     group_list = [group1, group2]
-#     session.commit()
-#
-#     for _ in range(50):
-#         full_name = faker.name().split(' ')
-#         age = faker.random.randint(16, 25)
-#         address = faker.address()
-#         group = faker.random.choice(group_list)
-#         student = Student(full_name, age, address, group.id)
-#         session.add(student)
-#
-#     session.commit()
-#     session.close()
--- test_structure/test_file.py
+++ test_structure/test_file.py
@@ -5,6 +5,6 @@ from test_structure.database import Session
 
 def go_test():
     session = Session()
-    user1 = User(name='John', fullname='Shchudlo')
+    user1 = User(name='Canny', fullname='Shchudlo')
     session.add(user1)
     session.commit()
