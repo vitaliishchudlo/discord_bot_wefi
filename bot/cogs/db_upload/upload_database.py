1a5ad17a27e3bbd2faeab6425ad627764fec2abe
vitaliishchudlo <vitalik.shchudlo01@ukr.net>
2023-06-27 22:56:15 +0300

    translated commentes into the English

--- bot/cogs/db_upload/upload_database.py
+++ bot/cogs/db_upload/upload_database.py
@@ -19,16 +19,16 @@ class UploadDatabaseCog(commands.Cog):
         await self.bot.wait_until_ready()
         logger.info(f'User: {ctx.author} entered the command: "upload_db"')
 
-        # Перевірка, чи є прикріплення до повідомлення
+        # Check if there are attachments to the message
         if len(ctx.message.attachments) == 0:
             logger.info(f'User: {ctx.author} tried to upload the database without attaching a file')
             await ctx.reply("You need to attach a file to the command")
             return
 
-        # Отримання першого прикріпленого файлу
+        # Get the first attached file
         attachment = ctx.message.attachments[0]
 
-        # Перевірка розширення та імені файлу
+        # Check the file extension and name
         valid_extensions = (".sqlite", ".db")
         valid_filename = "database"
         file_extension = os.path.splitext(attachment.filename)[1]
@@ -40,24 +40,24 @@ class UploadDatabaseCog(commands.Cog):
                 "Invalid file. Allowed extensions: **.sqlite**, **.db**. The file name should be **'database'**.")
             return
 
-        # Отримання шляху до папки бази даних
+        # Get the path to the database folder
         database_folder = "bot/database"
 
-        # Перевірка, чи існує папка бази даних
+        # Check if the database folder exists
         if not os.path.exists(database_folder):
             os.makedirs(database_folder)
 
-        # Збереження прикріпленого файлу в папку бази даних зі збереженням оригінального імені
+        # Save the attached file to the database folder with the original name
         file_path = os.path.join(database_folder, attachment.filename)
         await attachment.save(file_path)
 
         logger.info(f'User: {ctx.author} successfully uploaded the database: {attachment.filename}')
-        await ctx.reply(f"DataBase successfully uploaded. Path: {file_path}")
+        await ctx.reply(f"Database successfully uploaded. Path: {file_path}")
 
     @upload_db_command.error
     async def on_upload_db_command_error(self, ctx, error):
         logger.info(f'Error occurred while executing upload_db command: {error}')
-        await ctx.reply('You are not owner of the discord server')
+        await ctx.reply('You are not the owner of the Discord server')
 
 
 def register_cog(bot: Bot) -> None:
