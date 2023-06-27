47c7956421a07c57c63a191eba6fa2abb9a27e9e
vitaliishchudlo <vitalik.shchudlo01@ukr.net>
2023-06-27 22:50:22 +0300

    Added a few new loggers into the bot

--- bot/cogs/db_upload/upload_database.py
+++ bot/cogs/db_upload/upload_database.py
@@ -1,7 +1,12 @@
 import os
+from logging import getLogger
 
 from nextcord.ext import commands
 from nextcord.ext.commands import Bot
+
+from discord_bot_wefi.bot.misc.config import BotLoggerName
+
+logger = getLogger(BotLoggerName)
 
 
 class UploadDatabaseCog(commands.Cog):
@@ -11,8 +16,12 @@ class UploadDatabaseCog(commands.Cog):
     @commands.command(name='upload_db')
     @commands.is_owner()
     async def upload_db_command(self, ctx):
+        await self.bot.wait_until_ready()
+        logger.info(f'User: {ctx.author} entered the command: "upload_db"')
+
         # Перевірка, чи є прикріплення до повідомлення
         if len(ctx.message.attachments) == 0:
+            logger.info(f'User: {ctx.author} tried to upload the database without attaching a file')
             await ctx.reply("You need to attach a file to the command")
             return
 
@@ -26,6 +35,7 @@ class UploadDatabaseCog(commands.Cog):
         file_name = os.path.splitext(attachment.filename)[0]
 
         if file_extension not in valid_extensions or file_name != valid_filename:
+            logger.info(f'User: {ctx.author} tried to upload an invalid file: {attachment.filename}')
             await ctx.reply(
                 "Invalid file. Allowed extensions: **.sqlite**, **.db**. The file name should be **'database'**.")
             return
@@ -41,10 +51,12 @@ class UploadDatabaseCog(commands.Cog):
         file_path = os.path.join(database_folder, attachment.filename)
         await attachment.save(file_path)
 
-        await ctx.reply(f"DataBase successfully uploaded. Path:{file_path}")
+        logger.info(f'User: {ctx.author} successfully uploaded the database: {attachment.filename}')
+        await ctx.reply(f"DataBase successfully uploaded. Path: {file_path}")
 
     @upload_db_command.error
     async def on_upload_db_command_error(self, ctx, error):
+        logger.info(f'Error occurred while executing upload_db command: {error}')
         await ctx.reply('You are not owner of the discord server')
 
 
