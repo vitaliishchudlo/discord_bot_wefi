ec1cd3c46ce033e5077988267fa02da888e1b8a6
Vitalii <vitalik.shchudlo01@ukr.net>
2024-04-02 16:10:19 +0300

    Fixed the logger output

--- bot/tasks/runner.py
+++ bot/tasks/runner.py
@@ -2,11 +2,12 @@ import logging
 
 from nextcord.ext.commands import Bot
 
-from .user_activity import UserActivityTask, FaceitLvlTracker
+from .user_activity import UserActivityTask
+from .faceit_tracker import FaceitLvlTracker
 
 
 def start_all_loops(bot: Bot) -> None:
     UserActivityTask(bot).activity_voice_channels_check.start()
     FaceitLvlTracker(bot).faceit_lvl_check.start()
 
-    logging.info('Tasks (loops) functions starting... OK')
+    logging.info('[TASK] Tasks (loops) functions starting... OK')
