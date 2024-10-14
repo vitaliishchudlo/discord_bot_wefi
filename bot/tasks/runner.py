f7dad93dc3218fe20a596486d308df50480c8662
Vitalii Shchudlo <44679896+vitaliishchudlo@users.noreply.github.com>
2024-10-14 15:44:19 +0300

    Update runner.py

--- bot/tasks/runner.py
+++ bot/tasks/runner.py
@@ -7,7 +7,15 @@ from .faceit_tracker import FaceitLvlTracker
 
 
 def start_all_loops(bot: Bot) -> None:
-    UserActivityTask(bot).activity_voice_channels_check.start()
-    FaceitLvlTracker(bot).faceit_lvl_check.start()
+    user_activity_task = UserActivityTask(bot)
+    faceit_tracker_task = FaceitLvlTracker(bot)
+
+    if not user_activity_task.activity_voice_channels_check.is_running():
+        print(111, bool(user_activity_task.activity_voice_channels_check.is_running()))
+        user_activity_task.activity_voice_channels_check.start()
+
+    if not faceit_tracker_task.faceit_lvl_check.is_running():
+        print(222, bool(faceit_tracker_task.faceit_lvl_check.is_running()))
+        faceit_tracker_task.faceit_lvl_check.start()
 
     logging.info('[TASK] Tasks (loops) functions starting... OK')
